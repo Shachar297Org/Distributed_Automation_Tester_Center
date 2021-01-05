58e5ed43e9af8ae4e5127a6b9151e56dba90f351
eladkap <eladkapuza@gmail.com>
2021-01-05 12:59:34 +0200

    updated Event.cs

--- Console/Models/Event.cs
+++ Console/Models/Event.cs
@@ -6,6 +6,15 @@ namespace Console.Models
 {
     public class Event
     {
+        public Event(string ga, string sn, string ekey, string evalue, DateTime time)
+        {
+            EventDeviceType = ga;
+            EventDeviceSerialNumber = sn;
+            EventKey = ekey;
+            EventValue = evalue;
+            CreationTime = time;
+        }
+
         public string EventDeviceType { get; set; }
 
         public string EventDeviceSerialNumber { get; set; }
