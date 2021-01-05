54ce507ee55cbf2c90c8053e4d846e7654280c3f
Volodymyr Krytskyi <vkrytskyy@gmail.com>
2021-01-05 13:17:38 +0200

    Decorated event with JsonProperties

--- Console/Models/Event.cs
+++ Console/Models/Event.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,19 @@ namespace Console.Models
 {
     public class Event
     {
+        [JsonProperty("deviceType")]
         public string EventDeviceType { get; set; }
 
+        [JsonProperty("deviceSerialNumber")]
         public string EventDeviceSerialNumber { get; set; }
 
+        [JsonProperty("entryKey")]
         public string EventKey { get; set; }
 
+        [JsonProperty("entryValue")]
         public string EventValue { get; set; }
 
+        [JsonProperty("entryTimeStamp")]
         public DateTime CreationTime { get; set; }
     }
 }
